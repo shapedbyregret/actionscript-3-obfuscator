9598f2446b13262b8ab411e403864f6422f75205
scy <scy@scy-laptop.(none)>
2008-12-25 20:25:39 -0600

    Added inline block comment removal Tweaked other comment removals Added file name change option, but have not tested yet

--- obfuscator.py
+++ obfuscator.py
@@ -3,8 +3,20 @@
 import re
 
 # Declare variables
-removeComments = True;
-removeWhitespace = True;
+removeComments = True
+removeWhitespace = False
+changeFileName = True
+blockCommentFound = False
+fileName = "Main"
+newFileName = "Main_Obfs"
+
+# Function declarations
+def stripWhiteSpace(someText):
+	tmpLine = someText.strip() # Remove whitespace
+	tmpLine = tmpLine.rsplit("\n",1) # Remove carriage return
+	tmpLine = tmpLine[0].split("\t") # Remove tabs
+	tmpLine = "".join(["%s" % k for k in tmpLine]) # Iterate through tmpLine and joint it to ""
+	return tmpLine
 
 # Open file to change in read mode
 fileToChange = open("Main.as", "r")
@@ -18,25 +30,59 @@ for line in listLines:
 
 	# Remove comments
 	if removeComments:
-		commentIndex = line.rfind("//") # Finds last instance of "//" and returns the index
+		
+		# Remove and delete all inline block comments
+		tmpLine = ""
+		newLineCopy = newLine
+		blockBeginCount = newLineCopy.count("/*")
+		blockEndCount = newLineCopy.count("*/")
+		while blockBeginCount>0 and blockEndCount>0:
+			tmpList = newLineCopy.split("/*", 1)
+			tmpLine += tmpList[0]
+			tmpList = newLineCopy.split("*/",1)
+			tmpLine += tmpList[1]
+			newLineCopy = tmpLine
+			blockBeginCount -= 1
+			blockEndCount -= 1
+		if tmpLine != "":
+			newLine = tmpLine
+
+		# Remove all other block comments
+		if newLine.find("/*") != -1: # Beginning of a block comment has been found
+			blockCommentFound = True
+
+		if blockCommentFound:
+			if newLine.find("*/") != -1: # Found end of block comment
+				tmpLine = newLine.rsplit("*/", 1) # Get everything past block comment end
+				newLine = tmpLine[1]
+				blockCommentFound = False
+			else:
+				newLine = "\n" # Delete everything and replace with newline to preserve code structure
+
+		# Remove single line comments
+		strippedLine = stripWhiteSpace(newLine)
+		commentIndex = strippedLine.rfind("//") # Finds last instance of "//" and returns the index
 		if commentIndex != -1:
 			safeToDelete = True
-			semicolonIndex = line.find(";")
+			semicolonIndex = strippedLine.find(";")
 			if semicolonIndex != -1: # Semi-colon found
-				if commentIndex<semicolonIndex: # Before semi-colon, not safe to delete
+				if commentIndex<semicolonIndex and commentIndex !=0: # Before semi-colon and not beginning of line, not safe to delete
 					safeToDelete = False
 			if safeToDelete:
 				tmpLine = newLine.rsplit("//",1) # Split at last instance of "//", stores desired output into tmpLine[0]
 				tmpLine[0] += "\n" # Add a return carriage to preserve code structure
 				newLine = tmpLine[0] # Store our temp line into newLine
 	
-	# Remove whitespace and newlines
+	# Remove whitespace, newlines, and tabs
 	if removeWhitespace:
-		newLine.strip() # Remove whitespace
-		tmpLine = newLine.rsplit("\n",1) # Remove carriage return
-		tmpLine2 = tmpLine[0].split("\t") # Remove tabs
-		newLine = "".join(["%s" % k for k in tmpLine2]) # Iterate over tmpLine2 and join it to ""
+		newLine = stripWhiteSpace(newLine)
 	
+	# Change constructor to match new file name
+	if changeFileName:
+		if newLine.find(fileName) != -1:
+			newLine.replace(fileName, newFileName, 1)
+
+
 	# Replace line
 	lineIndex = listLines.index(line) # Get index of line we are currently working on
 	listLines.remove(line) # Remove current line and...
@@ -44,7 +90,12 @@ for line in listLines:
 
 
 # Write to file
-fileToWrite = open("Main_obfs.as", "w")
+if changeFileName:
+	newFileName += ".as"
+	fileToWrite = open(newFileName, "w")
+else
+	fileName += ".as"
+	fileToWrite = open(fileName, "w")
 for line in listLines:
 	fileToWrite.write(line)
 
