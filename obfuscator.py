7436dbfe4535d7aff9fe5a672a883d18583a6a69
scy <scy@scy-laptop.(none)>
2009-01-04 03:53:27 -0600

    Added string encoding. Current bug replaces parts of string that has already been replaced.

--- obfuscator.py
+++ obfuscator.py
@@ -2,30 +2,57 @@
 
 import re
 import random
+import sys
 
-# Declare variables
+# Options
 removeComments = True
-removeWhitespace = False
+removeWhitespace = True
 changeFileName = True
 changeVarName = True
+encodeStrings = True
 
+# Declare Variables
 blockCommentFound = False
 varDeclareFound = False
-fileName = "Main"
-newFileName = "Main_Obfs"
 varsFound = []
 varsNew = []
 
 # Function declarations
 def stripWhiteSpace(someText):
-	tmpLine = someText.strip() # Remove whitespace
-	tmpLine = tmpLine.rsplit("\n",1) # Remove carriage return
-	tmpLine = tmpLine[0].split("\t") # Remove tabs
-	tmpLine = "".join(["%s" % k for k in tmpLine]) # Iterate through tmpLine and joint it to ""
+	#tmpLine = someText.strip() # Remove whitespace
+	tmpLine = someText.replace("\n", "", 1) # Remove newlines
+	tmpLine = tmpLine.split("\t") # Remove tabs
+	tmpLine = "".join(["%s" % k for k in tmpLine]) # Iterate through tmpLine and join it to ""
 	return tmpLine
 
+def stringToHex(oldString):
+	newString = ""
+	for char in oldString: # Iterate over each char in given string
+		tmpString = hex(ord(char)) # Convert each char to unicode int, then convert int to hex string
+		tmpString = tmpString.replace("0", "\\", 1) # Replace first 0 in hex string with backslash
+		newString += tmpString # Append modified hex string to our newString
+	return newString
+
+# Check if arguments given
+if len(sys.argv)>1:
+	fullFileName = sys.argv[1]
+	fileName = fullFileName.rsplit(".", 1)[0]
+	if len(sys.argv)>2:
+		newFullFileName = sys.argv[2]
+		newFileName = newFullFileName.rsplit(".", 1)[0]
+	else:
+		tmpList = fullFileName.rsplit(".", 1)
+		if len(tmpList)>1:
+			newFullFileName = tmpList[0] + "_Obfs." + tmpList[1]
+		else:
+			newFullFileName = fileName + "_Obfs"
+		newFileName = fileName + "_Obfs"
+else:
+	print "Filename not provided."
+	exit()
+
 # Open file to change in read mode
-fileToChange = open("Main.as", "r")
+fileToChange = open(fullFileName, "r")
 listLines = fileToChange.readlines() # Store fileToChange to memory in a list
 fileToChange.close() # Close file
 
@@ -38,7 +65,7 @@ for line in listLines:
 	if removeComments:
 		
 		# Remove and delete all inline block comments
-		tmpLine = re.sub(r"(/\*).*(\*/)", "", newLine) # Matches everything between "/*" and "*/" and replaces with a newline
+		tmpLine = re.sub(r"(/\*).*?(\*/)", "", newLine) # Matches everything between "/*" and "*/" and replaces with a newline
 		if tmpLine != "":
 			newLine = tmpLine
 
@@ -75,30 +102,40 @@ for line in listLines:
 
 		varIndex = newLine.find("var") # See if "var" is declared on this line
 		functionIndex = newLine.find("function") # See if "function" found on this line
+		constIndex = newLine.find("const")
 
-		if varIndex != -1 or functionIndex != -1: # If "var" or "function" found, set varDeclareFound to True
+		if varIndex != -1 or functionIndex != -1 or constIndex != -1: # If "var", "const", or "function" found, set varDeclareFound to True
 			varDeclareFound = True
 
 		if varDeclareFound:
-			if varIndex == -1 and functionIndex != -1: # If function found but not var found
+			if functionIndex != -1: # If function found
 				tmpList = re.findall("\w+\s*(?=[\(|:])", newLine) # Make list of all words ending with ":" or "("
-			else: # Otherwise
+			else:
 				tmpList = re.findall("\w+\s*(?=:)", newLine) # Make list of all words ending with just ":"
 			for tmpLine in tmpList: # Sort through list of variable names
 				varsFound.append(tmpLine) # and append to varsFound list
-
+				
 				# Create new variable name
+				newVarName = ""
 				foundNewName = False
 				while not foundNewName: # Keep searching until find unique name
 					newVarName = "_" + str(random.randrange(1,5000)) # Create new name that is an underscore plus a random number up to 5000
 					if newVarName not in varsNew: # Make sure new name is unique
 						foundNewName = True
 				varsNew.append(newVarName)
+				#print tmpLine + " - " + newVarName
 
 			# Reached a semicolon or open bracket and presumably end of var/function declaration
 			if newLine.find(";") != -1 or newLine.find("{") != -1:
 				varDeclareFound = False
 
+	# Encode strings
+	if encodeStrings:
+		tmpList = re.findall(r"\".*?(?<!\\)\"", newLine) # Search for all double quotes
+		for literal in tmpList:
+			literal = literal.strip("\"")
+			newLine = newLine.replace(literal, stringToHex(literal))
+			#print newLine
 
 	# Remove whitespace, newlines, and tabs
 	if removeWhitespace:
@@ -130,11 +167,9 @@ if changeVarName:
 
 # Write to file
 if changeFileName:
-	newFileName += ".as" # Concatenate ".as" to the new file name
-	fileToWrite = open(newFileName, "w") # Then open it for writing
+	fileToWrite = open(newFullFileName, "w") # Then open it for writing
 else:
-	fileName += ".as"
-	fileToWrite = open(fileName, "w")
+	fileToWrite = open(fullFileName, "w")
 for line in listLines: # Go through our modified listLines one line at a time
 	fileToWrite.write(line) # Write each line to the file
 
